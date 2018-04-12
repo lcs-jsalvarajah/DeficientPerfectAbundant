bd94ed4640eae689b329828f1ae67eba0673b492
Salvarajah, Jheeven <jsalvarajah@lcs-mba3020.lcs.on.ca>
2018-04-12 19:32:21 -0400

    Still in progress

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -15,3 +15,30 @@ var rawInput = readLine()
 print("You said:")
 print(rawInput)
 
+var validInput = 0
+while 1==1 {
+    guard let givenInput = readLine() else {
+        print("Please Insert Inetger")
+        continue
+    }
+    guard let integerInput = Int(givenInput) else {
+        print("Please Insert Integer")
+        continue
+    }
+    if givenInput.count < 1 || givenInput.count > 32500 {
+        print("Please Stay in assigned integer count of between one and thirty-two thousand five-hundred")
+    }
+    validInput = integerInput
+    break
+}
+
+
+//Process
+var storage = 0
+for i in 1...validInput - 1  {
+    if validInput % i == 0 {
+        storage += i
+    }
+}
+
+// compare stoage to validInput and the output is the answer!
